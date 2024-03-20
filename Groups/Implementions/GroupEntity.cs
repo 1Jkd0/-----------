d2cb04a028e0c2e8a0d5f552221a30427232eb97
1Jkd0 <disk.video00002@gmail.com>
2024-03-20 19:18:16 +0100

    1

--- Groups/Implementions/GroupEntity.cs
+++ Groups/Implementions/GroupEntity.cs
@@ -1,45 +1,8 @@
-using System;
-using System.Threading;
+using Groups.Implementions;
 
-namespace Groups.Implementions
+namespace Groups.Implementions;
+
+public class GroupEntity
 {
-    public class GroupEntity
-    {
-        private string _groupName;
-        private int _memberCount;
 
-        public GroupEntity(string groupName)
-        {
-            _groupName = groupName;
-            _memberCount = 0;
-        }
-
-        public string GroupName
-        {
-            get { return _groupName; }
-            set { _groupName = value; }
-        }
-
-        public int MemberCount
-        {
-            get { return _memberCount; }
-        }
-
-        // Атомарна функція для додавання члена групи
-        public void AddMemberAtomically()
-        {
-            Interlocked.Increment(ref _memberCount);
-        }
-
-        // Атомарна функція для видалення члена групи
-        public void RemoveMemberAtomically()
-        {
-            Interlocked.Decrement(ref _memberCount);
-        }
-
-        public void PrintMemberCount()
-        {
-            Console.WriteLine($"Current member count in group {_groupName}: {_memberCount}");
-        }
-    }
 }
--- Groups/Implementions/PersonEntity.cs
+++ Groups/Implementions/PersonEntity.cs
@@ -1,19 +1,6 @@
-namespace Groups.Implementions
+namespace Groups.Implementions;
+
+public class PersonEntity
 {
-    public class PersonEntity
-    {
-        public string Name { get; private set; }
-        public int Age { get; private set; }
 
-        public PersonEntity(string name, int age)
-        {
-            Name = name;
-            Age = age;
-        }
-
-        public override string ToString()
-        {
-            return $"{Name}, Age: {Age}";
-        }
-    }
 }
