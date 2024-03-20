9e024776155bc7b7e2a4054e2b374e996d9d9007
1Jkd0 <disk.video00002@gmail.com>
2024-03-20 19:24:59 +0100

    2

--- Groups/Implementions/PersonEntity.cs
+++ Groups/Implementions/PersonEntity.cs
@@ -1,6 +1,19 @@
-namespace Groups.Implementions;
-
-public class PersonEntity
+namespace Groups.Implementions
 {
+    public class PersonEntity
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
 
+        public PersonEntity(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, Age: {Age}";
+        }
+    }
 }
